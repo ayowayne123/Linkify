ead8e6a2d7b4851fe00dffbaf0257caaf9f3976c
ayowayne123 <ayowayne123@gmail.com>
2022-10-29 11:51:49 +0300

    testing

--- src/components/Links/Link.jsx
+++ src/components/Links/Link.jsx
@@ -8,7 +8,7 @@ function Link() {
     <div className='Link'>
         <Button btn_Id="btn__zuri"  as="a" href="https://training.zuri.team" text="Zuri Team"/>
         <Button  btn_Id="books" as="a" href="https://books.zuri.team"  text="Zuri Books"/>
-        <Button  btn_Id="book__python" as="a" href="https://whereisthemouse.com"  text="Python Books"/>
+        <Button  btn_Id="book__python" as="a" href=" https://books.zuri.team/python-for-beginners?ref_id=ayomidewayne"  text="Python Books"/>
         <Button btn_Id="pitch" as="a" href="https://background.zuri.team" text="Background Check for Coders"/>
         <Button btn_Id="book__design" as="a" href="https://books.zuri.team/design-rules" text="Design Books"/>
         <span className='bio'> 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,12 +5,12 @@ import { AiFillTwitterCircle,AiFillSlackCircle } from "react-icons/ai";
 
 function Profile() {
   return (
-    <div>
-        <img className="profile" src={profile} alt="Profile of Ayomide" width="88" height="88" />
+    <div className='profile_cont'>
+       <img className="profile" src={profile} alt="Profile of Ayomide" width="88" height="88" />
         <div className="profile_name">Ayomide Ogu</div>
         <div className='icons'>
-          <span className='twitter' id='twitter'>
-           <AiFillTwitterCircle/>  </span><span > @ayomide_wayne </span>
+        <a href="https://twitter.com/Ayomide_wayne"> <span className='twitter' id='twitter'>
+          <AiFillTwitterCircle/>  </span><span> @ayomide_wayne  </span></a>
           <span className='slack'> <AiFillSlackCircle/></span>
            </div>
 
