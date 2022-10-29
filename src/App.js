a14bcf857ab6c81e662817357a2c2ac95e237be4
ayowayne123 <ayowayne123@gmail.com>
2022-10-29 03:54:56 +0300

    started responsive

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Link from './components/Links/Link';
 function App() {
   return (
    
-    <div className="App">
+    <div className="App container">
       <> <Profile/> </> 
       <> <Link/>  </>
        <> <Footer/> </>
--- src/components/Links/Link.jsx
+++ src/components/Links/Link.jsx
@@ -7,11 +7,14 @@ function Link() {
   return (
     <div className='Link'>
         <Button btn_Id="btn__zuri"  as="a" href="https://training.zuri.team" text="Zuri Team"/>
-       <Button  btn_Id="books" as="a" href="https://books.zuri.team"  text="Zuri Books"/>
-       <Button  btn_Id="book__python" as="a" href="https://whereisthemouse.com"  text="Python Books"/>
-       <Button btn_Id="pitch" as="a" href="https://background.zuri.team" text="Background Check for Coders"/>
-       <Button btn_Id="book__design" as="a" href="https://books.zuri.team/design-rules" text="Design Books"/>
-       <span className='bio'><img src={Slack} alt='slack' /> <AiFillGithub/> </span>
+        <Button  btn_Id="books" as="a" href="https://books.zuri.team"  text="Zuri Books"/>
+        <Button  btn_Id="book__python" as="a" href="https://whereisthemouse.com"  text="Python Books"/>
+        <Button btn_Id="pitch" as="a" href="https://background.zuri.team" text="Background Check for Coders"/>
+        <Button btn_Id="book__design" as="a" href="https://books.zuri.team/design-rules" text="Design Books"/>
+        <span className='bio'> 
+           <a href="https://slack.com"> <img src={Slack} alt='slack' /></a> 
+           <a href="https://github.com/ayowayne123"> <AiFillGithub/></a> 
+           </span>
        
     </div>
   )
