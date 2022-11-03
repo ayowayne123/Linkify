08038c485024d94247b5da22ac25e66b35c31edf
ayowayne123 <ayowayne123@gmail.com>
2022-11-03 21:38:14 +0200

    feat: began working on form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-form'>
+        <span> 
+        <span> Contact Me</span>
+        <span> Hi there, contact me to ask me about anything you have in mind.</span>
+        </span>
+        <form className='main-form'>
+  <label>
+    First Name
+    <input type="text" id='first_name' name="fname" placeholder='Enter your first name'/>
+  </label>
+  <label>
+    Last Name
+    <input type="text" id='last_name' name="lname" placeholder='Enter your Last name'/>
+  </label>
+  <label>
+    Last Name
+    <input type="email" id='email' name="email" placeholder='yourname@gmail.com'/>
+  </label>
+  <label>
+   Message
+    <textarea id='message' name='message' placeholder="Send me a message and I'll reply as soon as possible ..."  rows={5}/>
+    </label>
+  <input type="submit" value="Submit" />
+</form>
+        
+    </div>
   )
 }
 
