5ddf095d75b2544ab3f0094f1274bb88c751ac06
ayowayne123 <ayowayne123@gmail.com>
2022-11-05 09:30:26 +0200

    stylr: styling contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,29 +3,29 @@ import React from 'react'
 function Contact() {
   return (
     <div className='contact-form'>
-        <span> 
-        <span> Contact Me</span>
-        <span> Hi there, contact me to ask me about anything you have in mind.</span>
-        </span>
-        <form className='main-form'>
-  <label>
-    First Name
-    <input type="text" id='first_name' name="fname" placeholder='Enter your first name'/>
-  </label>
-  <label>
-    Last Name
-    <input type="text" id='last_name' name="lname" placeholder='Enter your Last name'/>
-  </label>
-  <label>
-    Last Name
-    <input type="email" id='email' name="email" placeholder='yourname@gmail.com'/>
-  </label>
-  <label>
-   Message
-    <textarea id='message' name='message' placeholder="Send me a message and I'll reply as soon as possible ..."  rows={5}/>
-    </label>
-  <input type="submit" value="Submit" />
-</form>
+    <span className='contact-me'> 
+        <span className='top'> Contact Me</span>
+        <span className='top-little'> Hi there, contact me to ask me about anything you have in mind.</span>
+    </span>
+    <form className='main-form' >
+        <label className='half'  >
+        <span class="label-text">First Name</span>
+            <input type="text" id='first_name' name="fname" placeholder='Enter your first name'  required/>
+        </label>
+        <label className='half'>
+        <span class="label-text">Last Name</span>
+            <input type="text" id='last_name' name="lname" placeholder='Enter your Last name' required f/>
+        </label>
+        <label>
+        <span class="label-text">Email</span>
+            <input type="email" id='email' name="email" placeholder='yourname@gmail.com' required/>
+        </label>
+        <label>
+        <span class="label-text">Message</span>
+            <textarea id='message' name='message' placeholder="Send me a message and I'll reply as soon as possible ..."  rows={5} required/>
+            </label>
+        <input type="submit" className='submit' id='btn__submit' value="Send Message" />
+    </form>
         
     </div>
   )
